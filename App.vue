a02995a1356753d24e42b265f96fa663900a7352
lsk <longsk@casc.ac.cn>
2020-10-23 16:21:53 +0800

    first-commit

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+	globalData:{
+		appName:'袭明',
+	},
+	onLaunch: function() {
+		console.log('App Launch111');
+	},
+	onShow: function() {
+		console.log('App Show');
+	},
+	onHide: function() {
+		console.log('App Hide');
+	}
+};
+</script>
+
+<style lang="scss">
+@import url('./style/common.scss');
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="index-page">
+		<scroll-view scroll-x="true" class="nav-bar-cont">
+			<view class="nav-item" @click="getFocus"><text>关注</text></view>
+			<view class="nav-item"><text>推荐</text></view>
+			<view class="nav-item"><text>活动</text></view>
+			<view class="nav-item"><text>政策</text></view>
+			<view class="nav-item"><text>招商</text></view>
+		</scroll-view>
+		<view class="container">33333</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			// #ifdef MP-WEIXIN||H5
+			uni.showToast({
+				title: '3333'
+			});
+			// #endif
+		},
+		methods: {
+			getFocus(){
+				console.log('点击了关注')
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import '../../style/index.scss';
+</style>
